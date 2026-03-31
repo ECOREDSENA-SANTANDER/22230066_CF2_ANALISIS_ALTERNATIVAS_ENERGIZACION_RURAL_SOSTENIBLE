179c7b14cbeb4736971409ebd566503b5d6d31ee
PABG1990 <pedro19908454@gmail.com>
2026-03-31 10:38:05 -0500

    Add_video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -25,8 +25,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/wAl-_Mo6mDM?si=Zb8bGpnzboMv6AFW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Alternativas de energización rural – Introducción
 
 
 </template>
