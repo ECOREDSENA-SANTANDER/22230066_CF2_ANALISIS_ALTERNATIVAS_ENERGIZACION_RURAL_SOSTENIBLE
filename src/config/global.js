ff55c4607262931bfbef9bb4fc274e6ea830735e
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2026-05-21 13:05:24 -0500

    Act_cfa

--- src/config/global.js
+++ src/config/global.js
@@ -67,7 +67,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/22230066_CF2_DU.pdf',
       },
       {
         icono: 'fas fa-download',
